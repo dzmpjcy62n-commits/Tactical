763c8ccb341fa09c46f50079ae7ba12d291f2c5e
dzmpjcy62n-commits <dzmpjcy62n@privaterelay.appleid.com>
2026-08-13 21:04:05 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='tactical-v3-20260813b';const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let z=r.clone();caches.open(CACHE).then(c=>c.put(e.request,z));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
